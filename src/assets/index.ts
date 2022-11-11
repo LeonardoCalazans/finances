bc09cb27e9c5c9c4fb0536829655e060bccc61b0
leonardoCalazans <leonardopintosilva@hotmail.com>
2022-11-11 16:15:00 -0300

    feat(SVG): creating sgv files

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+export { default as BalanceSvg } from './svg/BalanceSvg';
+export { default as DarkModeSvg } from './svg/DarkModeSvg';
+export { default as ExpenseSvg } from './svg/ExpenseSvg';
+export { default as IncomeSvg } from './svg/IncomeSvg';
+export { default as LightModeSvg } from './svg/LightModeSvg';
+export { default as RemoveSvg } from './svg/RemoveSvg';
+export { default as SettingsSvg } from './svg/SettingsSvg';
+export { default as GoogleSvg } from './svg/GoogleSvg';
---
+++ src/assets/svg/IncomeSvg.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Svg, { Path } from "react-native-svg";
+
+interface Props {
+  height?: string | number;
+  width?: string | number;
+  fill?: string;
+}
+
+const IncomeSvg = ({ height, width, fill }: Props): JSX.Element => {
+  return (
+    <Svg
+      height={height || 24}
+      viewBox="0 0 24 24"
+      width={width || 24}
+      fill={fill || "#18c901"}
+    >
+      <Path d="M0 0h24v24H0V0z" fill="none" />
+      <Path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z" />
+    </Svg>
+  );
+};
+
+export default IncomeSvg;
